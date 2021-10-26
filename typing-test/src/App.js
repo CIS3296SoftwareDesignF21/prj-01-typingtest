8d1a01d5caf8d52873f2574d63399fee0d523e4c
Colin Harker <53018170+ColinHarker@users.noreply.github.com>
2021-10-26 15:05:23 -0400

    Adding event listener with keystrokes

--- typing-test/src/App.js
+++ typing-test/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [key, setKey] = useState(0)
+
+  const onKeyPress = (event) => {
+    console.log(event.keyCode)
+    setKey(event.keyCode)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyPress);
+    };
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{String.fromCharCode(key)}</p>
       </header>
     </div>
   );
