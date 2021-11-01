126ef3e321d7037c0aa61aa4833a37c467b923af
Maafia Desktop <torn1998@gmail.com>
2021-11-01 17:39:21 -0400

    Temp Header File
    Not completed yet of course

---
+++ typing-test/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>className
+            {/* <HButton iconProp = ''/>
+            <HButton iconProp = ''/>
+            <HButton iconProp = ''/> */}
+        </div>
+    )
+}
