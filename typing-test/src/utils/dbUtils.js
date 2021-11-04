ad7ae9903f7d2cbcadf4b0e2c057b97bb80a84d5
Jason Lamanna <tun60633@temple.edu>
2021-11-04 00:43:26 -0400

    fixed SQL builder so INSERT works

--- typing-test/src/utils/dbUtils.js
+++ typing-test/src/utils/dbUtils.js
@@ -16,38 +16,52 @@ function endConn(){
   connection.end();
 }
 
-function sqlBuilder(type, values, table){
-  var sqlStr = "";
+function sqlBuilder(type, cols, values, table){
+  var sqlStr = '';
 
-  if(type == "i"){
-    sqlStr += "INSERT INTO typeTestdb."
-    sqlStr += table + " ";
-    for(var i = 0; i < values.length; i++){
-      if(i == values.length-1){
-        sqlStr += values[i];
+  if(type == 'i'){
+    sqlStr += 'INSERT INTO typeTestdb.' + table + ' (';
+    for(var j = 0; j < cols.length; j++){
+      if(j == cols.length-1){
+        sqlStr += cols[j];
       }else{
-      sqlStr += values[i] + ",";
+      sqlStr += cols[j] + ',';
       }
     }
+    sqlStr += ') VALUES (';
+    for(var i = 0; i < values.length; i++){
+      if(i == values.length-1){
+        sqlStr += '\'' + values[i] + '\'';
+      }else{
+      sqlStr += '\'' +values[i] + '\',';
+      }
+    }
+    sqlStr += ');';
   } 
   
-  else if(type == "s"){
-    sqlStr += "SELECT "
-    for(var i = 0; i < values.length; i++){
-      if(i == values.length-1){
-        sqlStr += values[i];
+  else if(type == 's'){
+    sqlStr += 'SELECT '
+    for(var i = 0; i < cols.length; i++){
+      if(i == cols.length-1){
+        sqlStr += cols[i];
       }else{
-      sqlStr += values[i] + ",";
+      sqlStr += cols[i] + ',';
       }
     }
-    sqlStr += " FROM typeTestdb." + table;
+    sqlStr += ' FROM typeTestdb.' + table;
   }
-  
+
   return sqlStr;
 }
 
 initCon();
-connection.query(sqlBuilder("s", ["*"], "user_table"), function (error, results) {
+
+connection.query(sqlBuilder('s', ['*'], [], 'user_table'), function (error, results) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
+
+connection.query(sqlBuilder('i', ['display_name' , 'user_email' , 'password'], ['aaa', 'test2', '5678'], 'user_table'), function (error, results) {
   if (error) throw error;
   console.log('The solution is: ', results);
 });
