953de1cbb2b9f55955710d2cc55ae21fb6c98f00
Jason Lamanna <tun60633@temple.edu>
2021-11-04 00:08:46 -0400

    making it function based, created SQL string builder

--- typing-test/src/utils/dbUtils.js
+++ typing-test/src/utils/dbUtils.js
@@ -1,16 +1,55 @@
-var mysql = require('mysql');
+//First Successful Connection/Query
 
-var connection = mysql.createConnection({
-  host     : 'typetest-db.cba2qmzidbbp.us-east-1.rds.amazonaws.com',
-  user     : 'admin',
-  password : 'typeTest123' 
-});
- 
-connection.connect();
- 
-connection.query('SELECT * FROM typeTestdb.user_table;', function (error, results, fields) {
+var mysql = require('mysql');
+var connection;
+
+function initCon(){
+  connection = mysql.createConnection({
+    host     : 'typetest-db.cba2qmzidbbp.us-east-1.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'typeTest123' 
+  });
+  connection.connect();
+}
+
+function endConn(){
+  connection.end();
+}
+
+function sqlBuilder(type, values, table){
+  var sqlStr = "";
+
+  if(type == "i"){
+    sqlStr += "INSERT INTO typeTestdb."
+    sqlStr += table + " ";
+    for(var i = 0; i < values.length; i++){
+      if(i == values.length-1){
+        sqlStr += values[i];
+      }else{
+      sqlStr += values[i] + ",";
+      }
+    }
+  } 
+  
+  else if(type == "s"){
+    sqlStr += "SELECT "
+    for(var i = 0; i < values.length; i++){
+      if(i == values.length-1){
+        sqlStr += values[i];
+      }else{
+      sqlStr += values[i] + ",";
+      }
+    }
+    sqlStr += " FROM typeTestdb." + table;
+  }
+  
+  return sqlStr;
+}
+
+initCon();
+connection.query(sqlBuilder("s", ["*"], "user_table"), function (error, results) {
   if (error) throw error;
   console.log('The solution is: ', results);
 });
- 
-connection.end();
+
+endConn();
