b813bc584ea31f8a0e3f6bf45c597fe21a928cd7
colin <colinharker5@yahoo.com>
2021-11-09 15:11:17 -0500

    Added stats component

---
+++ typing-test/src/components/Statistics.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withTheme } from 'styled-components'
+import '../stylesheets/Statistics.css'
+
+const Statistics = () => {
+    return (
+        <div className="stat-container">
+            I am the statistics page
+        </div>
+    )
+}
+
+export default Statistics
