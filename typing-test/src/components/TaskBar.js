ee49d41f194a27811f3204ad1f4f6cd876fb305c
colin <colinharker5@yahoo.com>
2021-11-09 15:12:40 -0500

    Taskbar icons now respond to what page you are on

--- typing-test/src/components/TaskBar.js
+++ typing-test/src/components/TaskBar.js
@@ -2,18 +2,18 @@ import React from 'react'
 import '../stylesheets/TaskBar.css'
 import {MdAccountCircle} from "react-icons/md"
 
-const TaskBar = () => {
+const TaskBar = ({page, setPage}) => {
     return (
         <div className="tb-wrapper">
             <div className="top-group">
-                <MdAccountCircle className='tb-button'/>
-                <MdAccountCircle className="tb-button"/>
-                <MdAccountCircle className='tb-button'/>
-                <MdAccountCircle className="tb-button"/>
+                <MdAccountCircle onClick={() => setPage(0)} style={page === 0 ? { color: '#50E3C2', filter: 'drop-shadow(0px 0px 8px #50E3C2)'} : null} className='tb-button'/>
+                <MdAccountCircle onClick={() => setPage(1)} style={page === 1 ? { color: '#50E3C2', filter: 'drop-shadow(0px 0px 8px #50E3C2)'} : null} className="tb-button"/>
+                <MdAccountCircle onClick={() => setPage(2)} style={page === 2 ? { color: '#50E3C2', filter: 'drop-shadow(0px 0px 8px #50E3C2)'} : null} className='tb-button'/>
+                <MdAccountCircle onClick={() => setPage(3)} style={page === 3 ? { color: '#50E3C2', filter: 'drop-shadow(0px 0px 8px #50E3C2)'} : null} className="tb-button"/>
 
             </div>
             <div className="bottom-group">
-                <MdAccountCircle className="tb-button"/>
+                <MdAccountCircle onClick={() => setPage(4)} style={page === 4 ? { color: '#50E3C2', filter: 'drop-shadow(0px 0px 8px #50E3C2)'} : null} className="tb-button"/>
             </div>
         </div>
     )
