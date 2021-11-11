ca5b2d3b44afcb9ebb50134c072caa6d6c13b12c
colin <colinharker5@yahoo.com>
2021-11-11 13:23:17 -0500

    Settings page component created

---
+++ typing-test/src/components/Settings.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../stylesheets/Settings.css'
+
+const Settings = () => {
+    return (
+        <div className="s-wrapper">
+            <div className="s-title">
+                Settings
+            </div>
+            <br/>
+            <div className="s-section1">
+                Some settings
+            </div>
+        </div>
+    )
+}
+
+export default Settings
