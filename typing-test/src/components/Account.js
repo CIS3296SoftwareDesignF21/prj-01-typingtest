f31e265daa9ecb54c1bd1c1c9330c4893fa64d6a
colin <colinharker5@yahoo.com>
2021-11-11 13:22:54 -0500

    Adding account statistics page

---
+++ typing-test/src/components/Account.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withTheme } from 'styled-components'
+import '../stylesheets/Account.css'
+
+const Account = () => {
+    return (
+        <div className="stat-container">
+            I am the statistics page
+        </div>
+    )
+}
+
+export default Account
